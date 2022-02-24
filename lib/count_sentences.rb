b39d3404ceff467c499d2ad3cd5f285612c65899
nathalia <nathaliatroina@gmail.com>
2022-02-24 16:47:26 -0500

    commit tree e887891cd88c148097367f389a876e5ff23a6c69

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")   
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
+
+# outro jeito de chegar ao mesmo resultado:
+
+# def count_sentences
+#   count = 0
+#   self.split(' ').each do |word| 
+#     if word.sentence?
+#       count+=1
+#     elsif word.question?
+#       count+=1
+#     elsif word.exclamation?
+#       count+=1
+#     end
+#   end
+#   count
+# end
+# end
